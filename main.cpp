467e71d26526b86a017dea7fa0dc916701862318
nguyentrungduc <nguyentrungduc08@gmail.com>
2018-06-28 17:07:32 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: cpu10664-local
+ *
+ * Created on June 28, 2018, 5:04 PM
+ */
+
+#include <cstdlib>
+#include <kchashdb.h>
+
+using namespace std;
+using namespace kyotocabinet;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    HashDB db;
+    return 0;
+}
+
