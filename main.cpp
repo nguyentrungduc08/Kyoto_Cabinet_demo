7fbbcf0eca72d9f2d82b1e6fef83316513002a8c
nguyentrungduc <nguyentrungduc08@gmail.com>
2018-07-06 09:12:28 +0700

    testing

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
  * File:   main.cpp
  * Author: cpu10664-local
  *
- * Created on June 28, 2018, 5:04 PM
+ * Created on July 4, 2018, 11:21 AM
  */
-
+#include <bits/stdc++.h>
 #include <cstdlib>
 #include <kchashdb.h>
 
@@ -14,9 +14,34 @@ using namespace kyotocabinet;
 /*
  * 
  */
-int main(int argc, char** argv) {
 
+int main(int argc, char** argv) {
     HashDB db;
+
+    if (db.open("dbID.kch", HashDB::OWRITER | HashDB::OCREATE)) {
+        std::cout << "open KC database success" << std::endl;
+    } else {
+        std::cerr << "open error: " << db.error().name() << std::endl;
+    }
+
+    string key = "key4";
+    string value = "value422";
+    
+//    db.set(key,value);
+    db.add(key, value);
+    
+    string res;
+    db.get(key, &res);
+    
+    cout << "output: " << res << endl;
+    cout << "total records: " << db.count() << endl;
+    
+    
+    if (db.close()) {
+        std::cout << "close KC database success" << std::endl;
+    } else {
+        std::cerr << "close error: " << db.error().name() << std::endl;
+    }
     return 0;
 }
 
